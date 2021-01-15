8f1b76e0c434e7ca6d2079e681ca919f836c434c
AfsanaRiza <74722943+AfsanaRiza@users.noreply.github.com>
2021-01-15 22:51:23 +0600

    Add files via upload

---
+++ testing.py
@@ -0,0 +1,8 @@
+import pytest
+from unittest import TestCase
+
+class TryTestin(TestCase):
+    def test_view_mynotes(self):
+        self.assertTrue(True)
+    def test_view_mynotes(self):
+        self.assertTrue(False)
